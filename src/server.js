b180d1b5481454dd6f2a645689b4a2f796670fe1
Hammad-87 <pakswim@gmail.com>
2020-07-20 13:04:03 +0200

    Server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const listEndpoints = require("express-list-endpoints");
 const cors = require("cors");
 const { join } = require("path");
+const mongoose = require("mongoose");
+const postRoutes = require("./routes/post")
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3003;
 const publicPath = join(__dirname, "../public");
 
 const server = express();
@@ -11,7 +13,17 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(express.static(publicPath));
+server.use("/post", postRoutes)
 
-server.listen(port, () => {
-  console.log(`Server running on port : ${port}`);
-});
+
+mongoose
+.connect("mongodb://localhost:27017/LinkedinBackend_TBW", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(
+  server.listen(port, () => {
+    console.log(`Server running on port : ${port}`);
+  })
+)
+.catch((err) => console.log(err))
